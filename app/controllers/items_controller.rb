5567f01e5d556d5de88036abf766f49f5289916f
Daniel Rice <danielrice5627@gmail.com>
2023-05-23 18:29:29 -0700

    commit tree ffd6c3a3a4eaba8568449cd10e86671e5aedb382

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,49 @@
 class ItemsController < ApplicationController
+  wrap_parameters format: []
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
-  def index
-    items = Item.all
-    render json: items, include: :user
+  #GET /users/:user_id/items/:id
+  def show
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      item = user.items.find(params[:id])
+      render json: item, status: :ok
+    else
+      item = Item.find(params[:id])
+      render json: item, include: :user
+    end
   end
 
+  #GET /users/:user_id/items
+  def index
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      return render json: user.items, status: :ok
+    else
+      items = Item.all
+      render json: items, include: :user
+    end
+  end
+
+  #POST /users/:user_id/items/:id
+  def create
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      new_item = user.items.create(item_params)
+      return render json: new_item, status: :created
+    else
+      item = Item.create(item_params)
+      render json: item, status: :created
+    end
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :price, :user_id, :item)
+  end
+
+  def render_not_found_response(invalid)
+    render json: {errors: invalid}, status: :not_found
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,17 @@
 class UsersController < ApplicationController
+  wrap_parameters format: []
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
+  #GET /users and 
   def show
-    user = User.find_by(id: params[:id])
-    render json: user, include: :items
+      user = User.find(params[:id])
+      render json: user, include: :items, status: :ok
+  end
+
+  private
+
+  def render_not_found_response(invalid)
+    render json: {errors: invalid}, status: :not_found
   end
 
 end
